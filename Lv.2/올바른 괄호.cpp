f11cdce51803ec2ab3af862dbbd29a84d211d1ff
truebird2201 <oy7405@naver.com>
2023-12-03 23:17:40 +0900

    [fix] 올바른 괄호 주석 추가

--- Lv.2/올바른 괄호.cpp
+++ Lv.2/올바른 괄호.cpp
@@ -10,7 +10,7 @@ bool solution(string s)
     {
         if (c == '(') check++;
         else check--;
-        if (check < 0)return false;
+        if (check < 0) return false;    // 0보다 작아지는 순간 )이 더 많아지므로 올바르지 않아짐
     }
-    return !check;
+    return !check;      // check가 0이 되지 않으면 괄호의 짝이 맞지 않으므로 0일때만 true
 }
